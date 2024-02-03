7d787e450637ddf765c337b664efd4b89325692d
Dayana Uzunova <daqnauzunova@abv.bg>
2024-02-03 20:01:15 +0200

    Exercises with Objects.
    Exercises with Objects.

---
+++ Objects and Composition - Lecture Exercises/cityTaxes_3.js
@@ -0,0 +1,18 @@
+function city(name, population, treasury){
+    const result = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes(){
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage){
+            this.population += Math.floor(this.population * percentage / 100);
+        },
+        applyRecession(percentage){
+            this.treasury -= Math.ceil(this.treasury * percentage / 100);
+        }
+    };
+    return result;
+}
---
+++ Objects and Composition - Lecture Exercises/objectFactory_4.js
@@ -0,0 +1,12 @@
+function factory(library, orders){
+    const result = [];
+
+    for (let order of orders){
+        const current = Object.assign({}, order.template);
+        for (let part of order.parts){
+            current[part] = library[part];
+        }
+        result.push(current);
+    }
+    return result;
+}
