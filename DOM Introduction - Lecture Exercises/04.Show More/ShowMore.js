92e28cdc4d14d169c889a1cc573f1ec70a9f3892
Dayana Uzunova <daqnauzunova@abv.bg>
2024-02-06 18:41:40 +0200

    Exercises with DOM.
    Exercises with DOM.

---
+++ DOM Introduction - Lecture Exercises/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let word = document.getElementById('text');
+    word.style.display = 'inline';
+    let wordToHide = document.getElementById('more');
+    wordToHide.style.display = 'none';
+}
