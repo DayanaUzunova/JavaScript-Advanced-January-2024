835b7cce2ae15f501808966dfef0fc6943c634cd
Dayana Uzunova <daqnauzunova@abv.bg>
2024-02-17 22:54:24 +0200

    Exercises with Classes.
    Exercises with Classes.

---
+++ Classes - Lecture Exercises/circle_3.js
@@ -0,0 +1,14 @@
+class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+    get diameter() {
+      return 2 * this.radius;
+    }
+    set diameter(diameter) {
+      this.radius = diameter / 2;
+    }
+    get area() {
+      return Math.PI * this.radius * this.radius;
+    }
+  }
